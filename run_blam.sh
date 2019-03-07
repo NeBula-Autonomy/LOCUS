0206ddaa293f8a1f3f733b100bd224ddeea1c1c0
meriemb <meriemb@student.ethz.ch>
2019-03-07 11:58:24 -0800

    build and run on ubuntu 18

--- run_blam.sh
+++ run_blam.sh
@@ -16,17 +16,17 @@ tmux split-window -h
 tmux send-keys -t 0 "roscore" C-m
 
 # Start ORBSLAM
-tmux send-keys -t 2 "sleep 1; source internal/devel/setup.bash;roslaunch blam_example test_online.launch" C-m
+tmux send-keys -t 2 "sleep 1; source ~/perception_ws/devel/setup.bash;roslaunch blam_example test_online.launch" C-m
 
 # Place rosbag
-tmux send-keys -t 1 "rosbag play -r 4 -s 2 /home/bjm/postdoc/data/Eagle_Mine/full_tunnel_hand-carry_no_rs_2_2019-01-19-laser_imu.bag" 
+tmux send-keys -t 1 "rosbag play -r 4 -s 2 /home/costar/Downloads/full_tunnel_hand-carry_no_rs_2_2019-01-19-laser_imu.bag" 
 
 
 # Prep tf record script
-tmux send-keys -t 3 "sleep 2; rviz -d internal/src/blam_example/rviz/lidar_slam.rviz" C-m
+tmux send-keys -t 3 "sleep 2; rviz -d /home/costar/perception_ws/src/localization/localizer_blam/internal/src/blam_example/rviz/lidar_slam.rviz" C-m
 
 # Prep close script
-tmux send-keys -t 4 "cd internal/src/human_loop_closure;source ../../devel/setup.bash" C-m
+tmux send-keys -t 4 "source /home/costar/perception_ws/devel/setup.bash; cd /home/costar/perception_ws/src/localizer/localizer_blam/internal/src/human_loop_closure" C-m
 tmux send-keys -t 4 "python manual_graph_edge.py "
 
 
