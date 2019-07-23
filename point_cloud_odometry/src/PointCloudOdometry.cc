803798c507d2564c39820c839d6ef96ac7d57ad4
andretag <andrea.tagliabue@me.com>
2019-07-23 16:42:52 -0700

    Update packages from master.

--- point_cloud_odometry/src/PointCloudOdometry.cc
+++ point_cloud_odometry/src/PointCloudOdometry.cc
@@ -83,13 +83,35 @@ bool PointCloudOdometry::LoadParameters(const ros::NodeHandle& n) {
 
   // Load initial position.
   double init_x = 0.0, init_y = 0.0, init_z = 0.0;
+  double init_qx = 0.0, init_qy = 0.0, init_qz = 0.0, init_qw = 1.0;
+  bool b_have_fiducial = true;
+  if (!pu::Get("fiducial_calibration/position/x", init_x))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/position/y", init_y))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/position/z", init_z))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/orientation/x", init_qx))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/orientation/y", init_qy))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/orientation/z", init_qz))
+    b_have_fiducial = false;
+  if (!pu::Get("fiducial_calibration/orientation/w", init_qw))
+    b_have_fiducial = false;
+
+  if (!b_have_fiducial) {
+    ROS_WARN("Can't find fiducials, using origin");
+  }
+
+  // convert initial quaternion to Roll/Pitch/Yaw
   double init_roll = 0.0, init_pitch = 0.0, init_yaw = 0.0;
-  if (!pu::Get("init/position/x", init_x)) return false;
-  if (!pu::Get("init/position/y", init_y)) return false;
-  if (!pu::Get("init/position/z", init_z)) return false;
-  if (!pu::Get("init/orientation/roll", init_roll)) return false;
-  if (!pu::Get("init/orientation/pitch", init_pitch)) return false;
-  if (!pu::Get("init/orientation/yaw", init_yaw)) return false;
+  gu::Quat q(gu::Quat(init_qw, init_qx, init_qy, init_qz));
+  gu::Rot3 m1;
+  m1 = gu::QuatToR(q);
+  init_roll = m1.Roll();
+  init_pitch = m1.Pitch();
+  init_yaw = m1.Yaw();
 
   gu::Transform3 init;
   init.translation = gu::Vec3(init_x, init_y, init_z);
