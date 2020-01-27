e39ce9241a0a7df6d7f4b054af10e19c5c9e1dc2
Jeremy Nash <jeremy.nash@jpl.nasa.gov>
2020-01-27 14:12:13 -0800

    Add test to compare multithreaded GICP to original GICP output

--- multithreaded_gicp/test/test_same_output_different_num_threads.cpp
+++ multithreaded_gicp/test/test_same_output_different_num_threads.cpp
@@ -1,3 +1,4 @@
+#include <pcl/registration/gicp.h>
 #include <multithreaded_gicp/gicp.h>
 #include <ros/package.h>
 #include <pcl/io/pcd_io.h>
@@ -19,10 +20,33 @@ int main(int argc, char **argv) {
     std::cerr << "Could not load reference_82_garage.pcd" << std::endl;
     return -1;
   }
+
+  double original_gicp_fitness_score;
+  Eigen::Matrix4f original_gicp_transform;
+  {
+    std::cout << "Original GICP" << std::endl;
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp; 
+    icp.setTransformationEpsilon(0.0000000001);
+    icp.setMaxCorrespondenceDistance(0.2);
+    icp.setMaximumIterations(20);
+    icp.setRANSACIterations(0);
+    icp.setMaximumOptimizerIterations(50);
+    icp.setInputSource(query);
+    icp.setInputTarget(reference);
+    pcl::PointCloud<pcl::PointXYZI> aligned_points;
+    icp.align(aligned_points);
+    original_gicp_transform = icp.getFinalTransformation();
+    std::cout << original_gicp_transform << std::endl;
+    original_gicp_fitness_score = icp.getFitnessScore();
+    std::cout << "Fitness score: " << original_gicp_fitness_score << std::endl;
+    std::cout << std::endl;
+  }
+
+  bool success = true;
   
   // Test gicp with num threads = 1 to 8
   for (int i = 1; i < 9; i++) {
-    pcl::MultithreadedGeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp; // (i, true);
+    pcl::MultithreadedGeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp; 
     icp.setTransformationEpsilon(0.0000000001);
     icp.setMaxCorrespondenceDistance(0.2);
     icp.setMaximumIterations(20);
@@ -35,7 +59,29 @@ int main(int argc, char **argv) {
     pcl::PointCloud<pcl::PointXYZI> aligned_points;
     icp.align(aligned_points);
     const Eigen::Matrix4f T = icp.getFinalTransformation();
-    std::cout << "Fitness score: " << icp.getFitnessScore() << std::endl;
+    std::cout << T << std::endl;
+    double fitness_score = icp.getFitnessScore();
+    std::cout << "Fitness score: " << fitness_score << std::endl;
+    if (T == original_gicp_transform) {
+      std::cout << "SUCCESS: Transform matches original GICP transform" << std::endl;
+    } else {
+      std::cerr << "FAILURE: Transform does not match original GICP transform" << std::endl;
+      success = false;
+    }
+
+    if (fitness_score == original_gicp_fitness_score) {
+      std::cout << "SUCCESS: Fitness score matches original GICP fitness score" << std::endl;
+    } else {
+      std::cerr << "FAILURE: Fitness score does not match original GICP fitness score" << std::endl;
+      success = false;
+    }
+    std::cout << std::endl;
+  }
+
+  if (success) {
+    std::cout << "All checks passed!" << std::endl;
+  } else {
+    std::cerr << "At least one check failed" << std::endl;
   }
 }
 
