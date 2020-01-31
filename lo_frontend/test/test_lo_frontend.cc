3cc99f16fe3528ef8f27a8f2d977059b33984984
Matteo Palieri <matteo.palieri@jpl.nasa.gov>
2020-01-31 12:49:58 -0800

    Add methods to be tested

--- lo_frontend/test/test_lo_frontend.cc
+++ lo_frontend/test/test_lo_frontend.cc
@@ -37,6 +37,34 @@ class LoFrontendTest : public ::testing::Test {
     bool SetDataIntegrationMode() {
       return lf.SetDataIntegrationMode();
     }
+ 
+    void ImuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg) {
+      lf.ImuCallback(imu_msg);
+    }
+
+    void OdometryCallback(const nav_msgs::Odometry::ConstPtr& odometry_msg) {
+      lf.OdometryCallback(odometry_msg);     
+    }
+
+    void PoseStampedCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_stamped_msg) {
+      lf.PoseStampedCallback(pose_stamped_msg);     
+    }
+
+    tf::Transform GetOdometryDelta(const nav_msgs::Odometry& odometry_msg) const {
+       return lf.GetOdometryDelta(odometry_msg);
+    }
+
+    Eigen::Matrix3d GetImuDelta() {
+      return lf.GetImuDelta();     
+    }
+
+    Eigen::Matrix3d GetImuYawDelta() {
+      return lf.GetImuYawDelta();     
+    }
+
+    void SwitchToImuIntegration() {
+      lf.SwitchToImuIntegration();     
+    }
 
   private:
 
