1f40b2c20c27714497d47688942709bf0462fe52
Matteo Palieri <matteo.palieri@jpl.nasa.gov>
2020-02-03 16:58:16 -0800

    Flat Ground Assumption 1.0

--- point_cloud_localization/src/PointCloudLocalization.cc
+++ point_cloud_localization/src/PointCloudLocalization.cc
@@ -279,9 +279,9 @@ bool PointCloudLocalization::MeasurementUpdate(const PointCloud::Ptr& query,
   }
   else {
     pose_update.translation = gu::Vec3(T(0, 3), T(1, 3), T(2, 3));
-    pose_update.rotation =    gu::Rot3(T(0, 0), T(0, 1), T(0, 2),
-                                       T(1, 0), T(1, 1), T(1, 2),
-                                       T(2, 0), T(2, 1), T(2, 2));
+    pose_update.rotation = gu::Rot3(T(0, 0), T(0, 1), T(0, 2),
+                                    T(1, 0), T(1, 1), T(1, 2),
+                                    T(2, 0), T(2, 1), T(2, 2));
   }
   
   // Only update if the transform is small enough
