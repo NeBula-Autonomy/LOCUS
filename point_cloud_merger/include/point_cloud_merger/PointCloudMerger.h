5ded846bd952870994e172bf3e8127916082faf9
Matteo Palieri <matteo.palieri@jpl.nasa.gov>
2020-05-04 22:42:00 -0700

    Add 3VLPs support in PointCloudMerger.h

--- point_cloud_merger/include/point_cloud_merger/PointCloudMerger.h
+++ point_cloud_merger/include/point_cloud_merger/PointCloudMerger.h
@@ -18,56 +18,62 @@
 
 class PointCloudMerger {
 
- public:
+  public:
 
-  typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
+    PointCloudMerger();
+    ~PointCloudMerger();
 
-  PointCloudMerger();
-  ~PointCloudMerger();
+    bool Initialize(const ros::NodeHandle& n);
 
-  bool Initialize(const ros::NodeHandle& n);
+    typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 
- private:
-
-  // Node initialization
-  bool LoadParameters(const ros::NodeHandle& n);
-  bool RegisterCallbacks(const ros::NodeHandle& n);
-  bool CreatePublishers(const ros::NodeHandle& n);
-
-  // Callback
-  void TwoPointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pcld1,
-                             const sensor_msgs::PointCloud2::ConstPtr& pcld2);
-
-  // Publish
-  void PublishMergedPointCloud(const PointCloud::ConstPtr combined_pc);
-
-  // The node's name
-  std::string name_;
-
-  // Subscribers
-  // Queue size of the approximate time policy that synchronizes the two point clouds.
-  int pcld_queue_size_{10};
-  // Filters
-  message_filters::Subscriber<sensor_msgs::PointCloud2>* pcld1_sub_;
-  message_filters::Subscriber<sensor_msgs::PointCloud2>* pcld2_sub_;
-  // Synchronization policy for the two point clouds
-  typedef message_filters::sync_policies::ApproximateTime<
-    sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> PcldSyncPolicy;
-  // Synchronizer for the two point clouds
-  typedef message_filters::Synchronizer<PcldSyncPolicy> PcldSynchronizer;
-  std::unique_ptr<PcldSynchronizer> pcld_synchronizer;
-
-  // Publishers
-  ros::Publisher merged_pcld_pub_;
+  private:
   
-  // Random downsample filter
-  bool b_use_random_filter_;
-  double decimate_percentage_;
+    bool LoadParameters(const ros::NodeHandle& n);
+    bool RegisterCallbacks(const ros::NodeHandle& n);
+    bool CreatePublishers(const ros::NodeHandle& n);
 
-  // Radius outlier filter
-  bool b_use_radius_filter_;
-  double radius_;
-  unsigned int radius_knn_;
+    void TwoPointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pcld1,
+                              const sensor_msgs::PointCloud2::ConstPtr& pcld2);  
+
+    void ThreePointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& pcld1,
+                                const sensor_msgs::PointCloud2::ConstPtr& pcld2, 
+                                const sensor_msgs::PointCloud2::ConstPtr& pcld3);
+
+    void PublishMergedPointCloud(const PointCloud::ConstPtr combined_pc);
+
+    std::string name_;
+
+    ros::Publisher merged_pcld_pub_;
+    
+    bool b_use_random_filter_;
+    double decimate_percentage_;
+
+    bool b_use_radius_filter_;
+    double radius_;
+    unsigned int radius_knn_;
+
+    // Approximate time policy queue size to synchronize point clouds
+    int pcld_queue_size_{10};
+    
+    message_filters::Subscriber<sensor_msgs::PointCloud2>* pcld1_sub_;
+    message_filters::Subscriber<sensor_msgs::PointCloud2>* pcld2_sub_;
+    message_filters::Subscriber<sensor_msgs::PointCloud2>* pcld3_sub_;  
+    
+    // 2 VLPs
+    typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::PointCloud2, 
+      sensor_msgs::PointCloud2> PcldSyncPolicy;
+    typedef message_filters::Synchronizer<PcldSyncPolicy> PcldSynchronizer;
+    std::unique_ptr<PcldSynchronizer> pcld_synchronizer;
+
+    // 3 VLPs
+    typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::PointCloud2, 
+      sensor_msgs::PointCloud2, 
+      sensor_msgs::PointCloud2> PcldSyncPolicy3;
+    typedef message_filters::Synchronizer<PcldSyncPolicy3> PcldSynchronizer3;
+    std::unique_ptr<PcldSynchronizer3> pcld_synchronizer3;
 
 };
 
