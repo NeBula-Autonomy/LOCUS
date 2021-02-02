348717d22e84a93bd11eb781fa23eddbace0bf4d
yunchang <yunchang@mit.edu>
2021-02-02 18:25:53 -0500

    add point to plane covariance calculation

--- point_cloud_localization/src/utils.cc
+++ point_cloud_localization/src/utils.cc
@@ -1,6 +1,6 @@
 #include <point_cloud_localization/utils.h>
 
-void addNormal(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
+void addNormal(const pcl::PointCloud<pcl::PointXYZI>& cloud,
                pcl::PointCloud<pcl::PointNormal>::Ptr& cloud_with_normals,
                const int search_radius) {
   pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
@@ -8,11 +8,11 @@ void addNormal(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(
       new pcl::PointCloud<pcl::PointXYZ>);
   // Convert point cloud XYZI to XYZ
-  for (int i; i < cloud->size(); i++) {
+  for (int i; i < cloud.size(); i++) {
     pcl::PointXYZ point;
-    point.x = cloud->points[i].x;
-    point.y = cloud->points[i].y;
-    point.z = cloud->points[i].z;
+    point.x = cloud.points[i].x;
+    point.y = cloud.points[i].y;
+    point.z = cloud.points[i].z;
     cloud_xyz->points.push_back(point);
   }
 
@@ -34,25 +34,26 @@ void addNormal(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
 
 // returns a point cloud whose centroid is the origin, and that the mean of
 // the distances to the origin is 1
-void normalizePCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr pclptr,
+void normalizePCloud(const pcl::PointCloud<pcl::PointXYZI>& cloud,
                      pcl::PointCloud<pcl::PointXYZI>::Ptr pclptr_normalized) {
   Eigen::Vector4f centroid_4d;
-  pcl::compute3DCentroid(*pclptr, centroid_4d);
+  pcl::compute3DCentroid(cloud, centroid_4d);
   Eigen::Vector3f centroid(centroid_4d.x(), centroid_4d.y(), centroid_4d.z());
 
   float dist = 0;
-  for (pcl::PointCloud<pcl::PointXYZI>::iterator it = pclptr->points.begin();
-       it != pclptr->points.end();
+  for (pcl::PointCloud<pcl::PointXYZI>::const_iterator it =
+           cloud.points.begin();
+       it != cloud.points.end();
        it++) {
     Eigen::Vector3f a_i(it->x, it->y, it->z);
     dist = dist + (a_i - centroid).norm();
   }
-  float factor = pclptr->points.size() / dist;
+  float factor = cloud.points.size() / dist;
   Eigen::Matrix4f transform;
   transform = Eigen::Matrix4f::Identity();
   transform.block(0, 0, 3, 3) = factor * Eigen::Matrix3f::Identity();
   transform.block(0, 3, 4, 1) = -factor * centroid_4d;
-  pcl::transformPointCloud(*pclptr, *pclptr_normalized, transform);
+  pcl::transformPointCloud(cloud, *pclptr_normalized, transform);
   pcl::compute3DCentroid(*pclptr_normalized, centroid_4d);
 
   // checkIfPCloudIsNormalized(pclptr_normalized);  //
